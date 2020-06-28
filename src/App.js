983c7b0697f5f0b18e4213d3f774c91338c36de3
Dimi95 <greek-princess95@hotmail.de>
2020-06-28 15:54:30 +0200

    Gruppentreffenaenderung vom 29.Juni, Erstellung von Grundgerüst, Logo und Bubble Chart mit React und Chart.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import logo from './PillsLogo.jpeg';
 import './App.css';
+import Chart from './components/Chart';
+/*import {scaleLinear} from 'd3-scale';*/
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Test
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      chartData:{}
+    }
+  }
+
+  componentWillMount(){
+    this.getChartData();
+  }
+
+  getChartData(){
+    this.setState({
+      chartData:{
+        labels: ['Age', 'Mari', 'Weed', 'Drugs', 'Pills', 'New High'],
+        datasets:[
+          {
+            label:'NewLabel',
+            data:[
+              617594,
+              181045,
+              153060,
+              106519,
+              105162,
+              95072
+            ],
+
+          
+            backgroundColor:[
+              'rgba(255, 99, 132, 0.6)',
+              'rgba(54, 162, 235, 0.6)',
+              'rgba(255, 206, 86, 0.6)',
+              'rgba(75, 192, 192, 0.6)',
+              'rgba(153, 102, 255, 0.6)',
+              'rgba(255, 159, 64, 0.6)',
+              'rgba(255, 99, 132, 0.6)'
+            ]
+          }
+          
+        ]
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Drug use by Age</h2>
+        </div>
+        <Chart chartData={this.state.chartData} location="Age" legendPosition="bottom"/>
+      </div>
+    );
+  }
+ /* d3.csv('drug-use-by-age.csv')
+  .then(makeChart);
+
+function makeChart(players) {
+}*/
 }
+
+
 
 export default App;
---
+++ src/components/Chart.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import {Bubble} from 'react-chartjs-2';
+
+class Chart extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      chartData:props.chartData
+    }
+  }
+
+  static defaultProps = {
+    displayTitle:true,
+    displayLegend: true,
+    legendPosition:'right',
+    location:'City'
+  }
+
+  render(){
+    return (
+      <div className="chart">
+        <Bubble
+          data={this.state.chartData}
+          options={{
+            title:{
+              display:this.props.displayTitle,
+              text:'Drug Use by '+this.props.location,
+              fontSize:25
+            },
+            legend:{
+              display:this.props.displayLegend,
+              position:this.props.legendPosition
+            }
+
+            
+          }}
+        />
+      </div>
+    )
+  }
+}
+
+export default Chart;
