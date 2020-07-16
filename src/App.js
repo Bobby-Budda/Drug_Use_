6769617cba2eac072a55e47f5f325642251522c3
Larin Talabani <lvrin@Larins-MBP.fritz.box>
2020-07-16 22:17:36 +0200

    added changes to drugusecontextfolder

--- src/App.js
+++ src/App.js
@@ -1,74 +1,72 @@
-import React, { Component } from 'react';
-import logo from './PillsLogo.jpeg';
-import './App.css';
-import Chart from './components/Chart';
+import React, { Component } from "react";
+import logo from "./PillsLogo.jpeg";
+import "./App.css";
+import Chart from "./components/Chart";
+
+import { DrugUseProvider } from "./drug-use-context/DrugUseProvider";
+import Dummy from "./Dummy";
+
 /*import {scaleLinear} from 'd3-scale';*/
 
-
-
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      chartData:{}
-    }
+      chartData: {},
+    };
   }
 
-  componentWillMount(){
+  componentWillMount() {
     this.getChartData();
   }
 
-  getChartData(){
+  getChartData() {
     this.setState({
-      chartData:{
-        labels: ['Age', 'Mari', 'Weed', 'Drugs', 'Pills', 'New High'],
-        datasets:[
+      chartData: {
+        labels: ["Age", "Mari", "Weed", "Drugs", "Pills", "New High"],
+        datasets: [
           {
-            label:'NewLabel',
-            data:[
-              617594,
-              181045,
-              153060,
-              106519,
-              105162,
-              95072
-            ],
+            label: "NewLabel",
+            data: [617594, 181045, 153060, 106519, 105162, 95072],
 
-          
-            backgroundColor:[
-              'rgba(255, 99, 132, 0.6)',
-              'rgba(54, 162, 235, 0.6)',
-              'rgba(255, 206, 86, 0.6)',
-              'rgba(75, 192, 192, 0.6)',
-              'rgba(153, 102, 255, 0.6)',
-              'rgba(255, 159, 64, 0.6)',
-              'rgba(255, 99, 132, 0.6)'
-            ]
-          }
-          
-        ]
-      }
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.6)",
+              "rgba(54, 162, 235, 0.6)",
+              "rgba(255, 206, 86, 0.6)",
+              "rgba(75, 192, 192, 0.6)",
+              "rgba(153, 102, 255, 0.6)",
+              "rgba(255, 159, 64, 0.6)",
+              "rgba(255, 99, 132, 0.6)",
+            ],
+          },
+        ],
+      },
     });
   }
 
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Drug use by Age</h2>
+      <DrugUseProvider>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="Logo" alt="logo" />
+            <h2>Drug use by Age</h2>
+          </div>
+          <Chart
+            chartData={this.state.chartData}
+            location="Age"
+            legendPosition="bottom"
+          />
         </div>
-        <Chart chartData={this.state.chartData} location="Age" legendPosition="bottom"/>
-      </div>
+        <Dummy></Dummy>
+      </DrugUseProvider>
     );
   }
- /* d3.csv('drug-use-by-age.csv')
+  /* d3.csv('drug-use-by-age.csv')
   .then(makeChart);
 
 function makeChart(players) {
 }*/
 }
-
-
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Logo />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
