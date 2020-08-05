396cc5fd1c1bac06ea418c76f5efe6b299d1a926
Larin Talabani <lvrin@Larins-MBP.fritz.box>
2020-08-05 19:35:58 +0200

    Abgabe Version

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
   const [DrugUseData] = useContext(DrugUseContext)
   return (
     <body>
-      <div class="container">
+      <div>
         <h1>Drug Use by Age</h1>
         <p>Percentage of those in an age group from 18-30 who used these drugtypes in the past 12 monthsb</p>
         <MyChart/>
